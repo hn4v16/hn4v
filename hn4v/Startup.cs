47b117196f72e260929b1394313efc61b8bf527b
KevinGarrett1 <mr.kevin.garrett@gmail.com>
2016-11-12 15:47:23 -0800

    Add project files.

---
+++ hn4v/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(hn4v.Startup))]
+namespace hn4v
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
